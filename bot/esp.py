de093cd363347fd2fe26fbc02922ce571efa1298
DzmitryKarpovich <63013639+DzmitryKarpovich@users.noreply.github.com>
2020-10-21 00:34:46 +0200

    95959595

--- bot/esp.py
+++ bot/esp.py
@@ -1,10 +1,8 @@
 import telebot
-
 bot = telebot.TeleBot("1230763312:AAGMtf6iWr27cu2iwBZTtsDzqLu3PjOrGhI")
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Привет. Я тестовый бот. " + '\n' + "Похоже, тебя втянули в какое-то обучение. Напиши что-нибудь ниже." + "\n\n" + "Type something below:")
-
 @bot.message_handler(content_types=['text'])
 def send_weather(message):
 	try:
@@ -15,5 +13,4 @@ def send_weather(message):
 		answer += 'Try again!'+ '\n'
 	bot.send_message(message.chat.id, answer)
 bot.polling( none_stop = True )
-
 exit()
