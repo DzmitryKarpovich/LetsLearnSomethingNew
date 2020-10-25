24b682eef9f80ba112f36cb6e506acaf4c44304f
DzmitryKarpovich <Dzmitry_Karpovich@epam.com>
2020-10-26 00:00:40 +0100

    wefwef

--- bot/esp.py
+++ bot/esp.py
@@ -1,6 +1,6 @@
 import telebot
 bot = telebot.TeleBot("1230763312:AAGMtf6iWr27cu2iwBZTtsDzqLu3PjOrGhI")
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start', 'help','wifemode'])
 def send_welcome(message):
 	bot.reply_to(message, "Привет. Я тестовый бот. " + '\n' + "Похоже, тебя втянули в какое-то обучение. Напиши что-нибудь ниже." + "\n\n" + "Type something below:")
 @bot.message_handler(content_types=['text'])
